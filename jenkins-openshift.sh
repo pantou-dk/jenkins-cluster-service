aaf4d1bad586938afc2e31fccd6fd5dabe3bcd68
Stephen Badger <stephen.badger@gmail.com>
2018-02-04 15:28:51 +0000

    Test command line arguments

--- jenkins-openshift.sh
+++ jenkins-openshift.sh
@@ -1,7 +1,9 @@
-#! /bin/bash -e
+#!/bin/bash -e
 
 if [ -f /usr/share/openshift/jenkins-plugins.txt ]; then
     /usr/local/bin/install-plugins.sh < /usr/share/openshift/jenkins-plugins.txt
 fi
+
+echo "$@"
 
 /usr/local/bin/jenkins.sh "$@"
