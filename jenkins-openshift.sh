56fb2e8a87d0433dd67c9f9f5824d33641d2521e
Stephen Badger <stephen.badger@gmail.com>
2018-02-07 17:45:11 +0000

    Update jenkins-openshift.sh

--- jenkins-openshift.sh
+++ jenkins-openshift.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 mkdir -p /var/jenkins_home/ref/plugins
+ln -s /var/jenkins_home/ref/plugins /usr/share/jenkins/ref/plugins
 
 if [ -d /usr/share/openshift/jenkins-plugins ]; then
     /usr/local/bin/install-plugins.sh < /usr/share/openshift/jenkins-plugins/*
