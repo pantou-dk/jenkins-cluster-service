6e79634c7efdc8b7d0dec44b4c0936055bf5684c
Stephen Badger <stephen.badger@gmail.com>
2018-02-07 16:53:38 +0000

    Remove debug message

--- jenkins-openshift.sh
+++ jenkins-openshift.sh
@@ -4,6 +4,4 @@ if [ -f /usr/share/openshift/jenkins-plugins.txt ]; then
     /usr/local/bin/install-plugins.sh < /usr/share/openshift/jenkins-plugins.txt
 fi
 
-echo "$@"
-
 /usr/local/bin/jenkins.sh "$@"
