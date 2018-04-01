2928e3240d25e91011a8181f36527fb6c161cf58
Stephen Badger <stephen.badger@gmail.com>
2018-04-01 20:13:33 +0100

    Copy default config.xml if missing

--- jenkins-openshift.sh
+++ jenkins-openshift.sh
@@ -4,4 +4,8 @@ if [ -f /usr/share/jenkins/ref/logging/log.properties ]; then
     export JAVA_OPTS="${JAVA_OPTS} -Djava.util.logging.config.file=/usr/share/jenkins/ref/logging/log.properties"
 fi
 
+if [ ! -f /var/jenkins_home/config.xml ]; then
+    cp /usr/share/jenkins/ref/config/config.xml /var/jenkins_home/config.xml
+fi
+
 /usr/local/bin/jenkins.sh "$@"
